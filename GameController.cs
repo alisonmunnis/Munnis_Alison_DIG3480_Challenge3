c66fb43b7dfc31c3782f5e6630b803616ef98241
alisonmunnis <46458925+alisonmunnis@users.noreply.github.com>
2019-04-07 16:13:26 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+   
+
+    public Text ScoreText;
+    public Text RestartText;
+    public Text GameOverText;
+    public Text winText;
+   
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start ()
+    {
+        gameOver = false;
+        restart = false;
+        RestartText.text = "";
+        GameOverText.text = "";
+        winText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+
+    }
+
+   void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                SceneManager.LoadScene("Munnis_Alison_DIG3480_Challenge3");
+            }
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        { 
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+
+            if (gameOver)
+            {
+                RestartText.text = "Press 'N' for new game";
+                restart = true;
+                break; 
+            }
+           
+        }
+
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void GameOver ()
+    {
+        GameOverText.text = "GAME CREATED BY ALISON MUNNIS";
+        gameOver = true;
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You win! GAME CREATED BY ALISON MUNNIS ";
+            gameOver = true;
+            restart = true;
+        }
+    }
+
+}
